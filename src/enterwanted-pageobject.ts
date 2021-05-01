519b86dc19db1f1e12c1bd2b8775f6562c2b11d3
Mahsa Ghahri <mahsagh@Mahsas-Air.attlocal.net>
2021-05-01 01:10:37 -0500

    adds jsDoc

--- src/enterwanted-pageobject.ts
+++ src/enterwanted-pageobject.ts
@@ -10,18 +10,30 @@ export class EnterWanted {
     constructor(driver) {
         this.driver = driver
     }
-    
+
+    /**
+     * This navigate to the enterwanted application and opens up the app
+     */
     async navigate() {
       await this.driver.get(this.url);
     }
-
+    /**
+     * 
+     * @param item This is the inputText string for header
+     * this function gets the input provided by tester and put it in header input, then clicks on submit button
+     * @returns {undefined}
+     */
     async submitHeaderText(item:string) {
       await this.driver.wait(until.elementLocated(this.headerInput));
       await this.driver.wait(until.elementLocated(this.submitbutton));
       await this.driver.findElement(this.headerInput).sendKeys(item);
       await (await this.driver.findElement(this.submitbutton)).click();
     }
-
+    /**
+     * 
+     * @returns first validation error message in the page, 
+     * it will be check in test code later to see if that belongs to header or not
+     */
     async getError() {
     await this.driver.wait(until.elementsLocated(this.errors));
     await this.driver.findElement(this.errors);
